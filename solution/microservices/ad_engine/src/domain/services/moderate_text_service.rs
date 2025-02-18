704aa962a119ef74fd5af40186090d479bd8182d
IOSHED <iveninvaala7@gmail.com>
2025-02-18 12:55:56 +0300

    Ref: Refactoring and add tests.

--- solution/microservices/ad_engine/src/domain/services/moderate_text_service.rs
+++ solution/microservices/ad_engine/src/domain/services/moderate_text_service.rs
@@ -29,41 +29,54 @@ impl ModerateTextService {
         if !is_activated {
             return Ok(text.to_vec());
         }
+
         let abusive_words = repo
             .get_words()
             .await
             .map_err(domain::services::ServiceError::Repository)?;
 
-        let result = text
+        Ok(text
             .iter()
-            .map(|original_str| {
-                let phrase = original_str.to_lowercase().replace(' ', "");
-                let filtered_phrase = self.filter_phrase(phrase);
+            .map(|original_str| self.mask_abusive_words(&self.filter_phrase(original_str.clone()), &abusive_words))
+            .collect())
+    }
 
-                let phrase_length = filtered_phrase.len();
+    fn mask_abusive_words(&self, original_str: &str, abusive_words: &[String]) -> String {
+        let words: Vec<&str> = original_str.split_whitespace().collect();
 
-                let has_abusive_content = abusive_words.par_iter().any(|word| {
-                    let word_length = word.len();
-                    if word_length == 0 || word_length > phrase_length {
-                        return false;
-                    }
+        words
+            .iter()
+            .map(|&word| self.mask_word(word, abusive_words))
+            .collect::<Vec<String>>()
+            .join(" ")
+    }
 
-                    (0..=phrase_length.saturating_sub(word_length)).any(|part| {
-                        let fragment: String = filtered_phrase.chars().skip(part).take(word_length).collect();
-                        self.levenshtein_distance(&fragment, word)
-                            <= (word_length as f32 * self.sensitivity).round() as usize
-                    })
-                });
+    fn mask_word(&self, word: &str, abusive_words: &[String]) -> String {
+        let cleaned_word = word.to_lowercase().replace(' ', "");
 
-                if has_abusive_content {
-                    "***".to_string()
-                } else {
-                    original_str.clone()
-                }
-            })
-            .collect();
+        if abusive_words
+            .par_iter()
+            .any(|abusive_word| self.is_abusive_word(&cleaned_word, abusive_word))
+        {
+            "***".to_string()
+        } else {
+            word.to_string()
+        }
+    }
 
-        Ok(result)
+    pub fn is_abusive_word(&self, cleaned_word: &str, abusive_word: &str) -> bool {
+        let word_length = abusive_word.len();
+        let cleaned_word_length = cleaned_word.len();
+
+        if word_length == 0 || word_length > cleaned_word_length {
+            return false;
+        }
+
+        (0..=cleaned_word_length.saturating_sub(word_length)).any(|part| {
+            let fragment: String = cleaned_word.chars().skip(part).take(word_length).collect();
+            self.levenshtein_distance(&fragment, abusive_word)
+                <= (word_length as f32 * self.sensitivity).round() as usize
+        })
     }
 
     pub async fn check_abusive_content<R: IGetAbusiveWords>(
@@ -71,42 +84,39 @@ impl ModerateTextService {
         text: &[String],
         is_activated: bool,
         repo: R,
-    ) -> domain::services::ServiceResult<()> {
+    ) -> domain::services::ServiceResult<bool> {
         if !is_activated {
-            return Ok(());
+            return Ok(false);
         }
+
         let abusive_words = repo
             .get_words()
             .await
-            .map_err(|e| domain::services::ServiceError::Repository(e))?;
+            .map_err(domain::services::ServiceError::Repository)?;
 
-        let phrase: String = text.join(" ").to_lowercase().replace(" ", "");
-        let filtered_phrase = self.filter_phrase(phrase);
-
-        let phrase_length = filtered_phrase.len();
-
-        let result = abusive_words.par_iter().find_map_any(|word| {
-            let word_length = word.len();
-            if word_length == 0 || word_length > phrase_length {
-                return None;
+        for original_str in text {
+            let filtered_str = self.filter_phrase(original_str.clone());
+            if self.contains_abusive_words(&filtered_str, &abusive_words) {
+                return Ok(true);
             }
-
-            (0..=phrase_length.saturating_sub(word_length)).find_map(|part| {
-                let fragment: String = filtered_phrase.chars().skip(part).take(word_length).collect();
-                if self.levenshtein_distance(&fragment, word)
-                    <= (word_length as f32 * self.sensitivity).round() as usize
-                {
-                    Some(word)
-                } else {
-                    None
-                }
-            })
-        });
-
-        if let Some(res) = result {
-            return Err(domain::services::ServiceError::Censorship(res.into()));
         }
-        Ok(())
+
+        Ok(false)
+    }
+
+    fn contains_abusive_words(&self, original_str: &str, abusive_words: &[String]) -> bool {
+        let words: Vec<&str> = original_str.split_whitespace().collect();
+
+        words.iter().any(|&word| self.is_abusive_word_vec(word, abusive_words))
+    }
+
+    fn is_abusive_word_vec(&self, word: &str, abusive_words: &[String]) -> bool {
+        let cleaned_word = word.to_lowercase().replace(' ', "");
+
+        abusive_words.iter().any(|abusive_word| {
+            self.levenshtein_distance(&cleaned_word, abusive_word)
+                <= (abusive_word.len() as f32 * self.sensitivity).round() as usize
+        })
     }
 
     fn filter_phrase(&self, phrase: String) -> String {
@@ -183,5 +193,96 @@ impl ModerateTextService {
                 "я": ["я", "ya"]
             }
         )
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct MockRepo {
+        words: Vec<String>,
+    }
+
+    #[async_trait]
+    impl IGetAbusiveWords for MockRepo {
+        async fn get_words(&self) -> infrastructure::repository::RepoResult<Vec<String>> {
+            Ok(self.words.clone())
+        }
+    }
+
+    #[test]
+    fn test_mask_abusive_words() {
+        let service = ModerateTextService::new(0.2);
+        let abusive_words = vec!["плохое".to_string()];
+
+        assert_eq!(
+            service.mask_abusive_words("Это не плохое", &abusive_words),
+            "Это не ***"
+        );
+        assert_eq!(
+            service.mask_abusive_words("Это не хорошее да да", &abusive_words),
+            "Это не хорошее да да"
+        );
+    }
+
+    #[test]
+    fn test_is_abusive_word() {
+        let service = ModerateTextService::new(0.3);
+
+        assert!(service.is_abusive_word("плохо", "плохо"));
+        assert!(service.is_abusive_word("плохое", "плохо"));
+        assert!(!service.is_abusive_word("плоховастенький", "плохо"));
+    }
+
+    #[tokio::test]
+    async fn test_hide_abusive_content() {
+        let service = ModerateTextService::new(0.2);
+        let repo = MockRepo {
+            words: vec!["плохо".to_string()],
+        };
+        let text = vec![
+            "Это, не хорошо а пло][о .выспасть".to_string(),
+            "Это ю хороше .но не".to_string(),
+        ];
+
+        let result = service.hide_abusive_content(&text, true, repo).await.unwrap();
+
+        assert_eq!(result, vec!["Это, не хорошо а *** .выспасть", "Это ю хороше .но не"]);
+    }
+
+    #[tokio::test]
+    async fn test_check_abusive_content() {
+        let service = ModerateTextService::new(0.3);
+        let repo = MockRepo {
+            words: vec!["плохо".to_string()],
+        };
+        let text = vec!["Не очнеь плоhо".to_string(), "Хя хя нет п".to_string()];
+
+        let result = service.check_abusive_content(&text, true, repo).await.unwrap();
+
+        assert!(result);
+
+        let repo = MockRepo {
+            words: vec!["плохо".to_string()],
+        };
+
+        let clean_text = vec!["Хях я нет".to_string()];
+        let result_clean = service.check_abusive_content(&clean_text, true, repo).await.unwrap();
+
+        assert!(!result_clean);
+    }
+
+    #[tokio::test]
+    async fn test_check_abusive_content_disabled() {
+        let service = ModerateTextService::new(0.3);
+        let repo = MockRepo {
+            words: vec!["badword".to_string()],
+        };
+        let text = vec!["This is a badword".to_string()];
+
+        let result = service.check_abusive_content(&text, false, repo).await.unwrap();
+
+        assert!(!result);
     }
 }
